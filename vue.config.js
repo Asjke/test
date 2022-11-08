8ac54a4dd93a423d0618ea02f5102d71b4d9fe09
Eugene Filimonov <filimonov-92@list.ru>
2022-11-08 13:28:02 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'composition/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? 'test/' : '/'
 }
